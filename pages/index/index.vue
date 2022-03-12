d9206710eb1fe4b7a8bb8aa239bc654b6eb0be74
guangnian <1332904017@qq.com>
2022-03-12 17:14:51 +0800

    测试提交

---
+++ pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="content">
+		<swiper indicator-dots circular>
+			<swiper-item v-for="item in swipers" :key="item.goods_id">
+				<image mode="widthFix" :src="item.image_src"></image>
+			</swiper-item>
+		</swiper>
+		<input type="text" placeholder-style="color:#cccccc;" style="width: 50%;height: 30rpx; border: 1rpx solid #007AFF;" :value="123123"/>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				swipers: []
+			}
+		},
+		onLoad() {
+			this.getSwipers()
+		},
+		methods: {
+			// 获取轮播图的数据
+			async getSwipers() {
+				const res = await this.$myRuquest({
+					url: '/api/public/v1/home/swiperdata'
+				})
+				this.swipers = res.data.message
+			},
+
+		}
+	}
+</script>
+
+<style lang="less">
+	.content {
+		swiper {
+			width: 750rpx;
+			height: 350rpx;
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
+</style>
---
+++ pages/news/news.vue
@@ -0,0 +1,11 @@
+<template>
+	<view>
+		<text>咨询</text>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
